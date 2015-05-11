432304bb02d23b22b1ac8c94c5352e5034dbbde9
Derek Battams <derek@battams.ca>
2015-05-11 19:31:11 -0400

    Use localhost for internal api client calls

--- src/main/webapp/WEB-INF/groovy/inbox.groovy
+++ src/main/webapp/WEB-INF/groovy/inbox.groovy
@@ -3,7 +3,7 @@ import groovyx.net.http.RESTClient
 if(params.clnt) {
 	def url = new URL(request.requestURL.toString())
 	def ctxPath = request.contextPath ? "/$request.contextPath" : ''
-	def rest = new RESTClient(new URL("$url.protocol://$url.host:${url.port}$ctxPath/api/"))
+	def rest = new RESTClient(new URL("$url.protocol://localhost:${url.port}$ctxPath/api/"))
 	request.setAttribute('metadata', rest.get(path: 'messages', query: params).data)
 	request.getRequestDispatcher('/inbox.html').forward(request, response)
 } else
