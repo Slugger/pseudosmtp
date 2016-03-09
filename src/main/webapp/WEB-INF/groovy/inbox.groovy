ebe73ce26b6766bfcd1e71d1174a3cc79b8938a2
Derek Battams <dbattams@ca.ibm.com>
2016-03-09 09:26:57 -0500

    Handle case where mailbox is empty (stop viewing from throwing 500)

--- src/main/webapp/WEB-INF/groovy/inbox.groovy
+++ src/main/webapp/WEB-INF/groovy/inbox.groovy
@@ -4,7 +4,16 @@ if(params.clnt) {
 	def url = new URL(request.requestURL.toString())
 	def ctxPath = request.contextPath ?: '/'
 	def rest = new RESTClient(new URL("$url.protocol://localhost:${url.port}$ctxPath/api/"))
-	request.setAttribute('metadata', rest.get(path: 'messages', query: params).data)
+	def data
+	try {
+		data = rest.get(path: 'messages', query: params).data
+		request.setAttribute('metadata', data)
+	} catch(HttpResponseException e) {
+		if(e.statusCode == 404) // empty mailbox
+			request.setAttbiute('metadata', [])
+		else
+			throw e
+	}
 	request.setAttribute('remoteHost', url.host)
 	request.getRequestDispatcher('/inbox.html').forward(request, response)
 } else
