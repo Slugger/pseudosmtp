6a64c57d0f127a46536fa63e964d162e61e1d657
Derek Battams <dbattams@ca.ibm.com>
2016-03-09 09:12:14 -0500

    Bug fixes
    1) Fix issue where deployment at non-root context would cause 500s
    2) Fix bug where links to MIME message always referred to localhost

--- src/main/webapp/WEB-INF/groovy/inbox.groovy
+++ src/main/webapp/WEB-INF/groovy/inbox.groovy
@@ -2,9 +2,10 @@ import groovyx.net.http.RESTClient
 
 if(params.clnt) {
 	def url = new URL(request.requestURL.toString())
-	def ctxPath = request.contextPath ? "/$request.contextPath" : ''
+	def ctxPath = request.contextPath ?: '/'
 	def rest = new RESTClient(new URL("$url.protocol://localhost:${url.port}$ctxPath/api/"))
 	request.setAttribute('metadata', rest.get(path: 'messages', query: params).data)
+	request.setAttribute('remoteHost', url.host)
 	request.getRequestDispatcher('/inbox.html').forward(request, response)
 } else
 	request.getRequestDispatcher('/login.html').forward(request, response)
